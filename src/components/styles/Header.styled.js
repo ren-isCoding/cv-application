887eab207d4f688e4c9a9b1d91e28d4779a3730b
renhubgit <renik2001@gmail.com>
2022-05-31 13:04:36 +0200

    header styling

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 export let StyledHeader = styled.div`
     font-family: "Bungee", cursive;
     display: flex;
+    justify-content: center;
     background-color: #333333;
     color: whitesmoke;
     padding: 2rem;
