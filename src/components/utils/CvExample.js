77928a1856baa9c2ddeb60856d05713b8d18a7e1
renhubgit <renik2001@gmail.com>
2022-06-01 16:57:07 +0200

    CvExample changes

--- src/components/utils/CvExample.js
+++ src/components/utils/CvExample.js
@@ -1,16 +1,39 @@
-import examplePhoto from "../assets/boris.png"
-import { v4 as uuidv4 } from "uuid"
+import examplePhoto from "../assets/examplePhoto.png"
 
 const CvExample = {
     personalInfo: {
-        fName: "",
-        lName: "",
-        profession: "",
-        email: "",
-        phoneNum: "",
-        desc: "",
+        fName: "Boris",
+        lName: "Dakoolcat",
+        profession: "Web Developer",
+        email: "boris123@gmail.com",
+        phoneNum: "123456789",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio error labore, sapiente recusandae fuga accusantium laudantium, ratione possimus quisquam ea, fugit obcaecati hic soluta. Omnis illo quos aspernatur rem ab.",
         photo: examplePhoto,
     },
+
+    education: [
+        {
+            id: "01b9db56-0fac-49a1-aef6-fcd4de5180db",
+            school: "Technical Institute",
+            schoolCity: "Naples",
+            degree: "High school diploma",
+            schoolStart: "2015",
+            schoolEnd: "2020",
+        },
+    ],
+
+    experience: [
+        {
+            id: "97e5bdec-41f2-4e8b-9612-25b710c9adc0",
+            job: "Junior Web Developer",
+            company: "Facebook",
+            jobCity: "Naples",
+            jobStart: "2020",
+            jobEnd: "2025",
+        },
+    ],
+
+    skills: [],
 }
 
 export default CvExample
